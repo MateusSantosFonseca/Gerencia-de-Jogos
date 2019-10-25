104821c1453aeac8417c9366ffe814ae915521e0
Mateus Santos Fonseca <45724993+MateusSantosFonseca@users.noreply.github.com>
2019-10-25 02:22:08 -0300

    adding models

---
+++ GerenciaGames/App_Code/FileUtilities.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.UI;
+
+public static class FileUtilities
+{
+
+    private readonly static string Path = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + "/jogos.txt";
+
+
+    public static List<Jogo> LerJogos()
+    {
+        List<Jogo> listaJogos = new List<Jogo>();
+
+
+        if (HaArquivoEJogos())
+        {
+            string[] jogosTexto = File.ReadAllLines(Path);
+            int i = 0;
+
+            while (i < jogosTexto.Length)
+            {
+                Jogo jogo = new Jogo(jogosTexto[i], jogosTexto[i + 1].Replace(" ", ""), jogosTexto[i + 2]);
+                listaJogos.Add(jogo);
+                i += 3;
+            }
+            //System.Diagnostics.Debug.WriteLine();
+        }
+
+        return listaJogos;
+    }
+
+    public static void AddJogoArquivo(Jogo jogo)
+    {
+
+        string[] jogoNovo = new string[3];
+        jogoNovo[0] = jogo.nome.Trim();
+        jogoNovo[1] = jogo.urlImagem.Trim();
+        jogoNovo[2] = jogo.sinopse.Trim();
+        AppendArquivo(jogoNovo);
+    }
+
+    public static bool HaArquivoEJogos()
+    {
+        return (File.Exists(Path) && File.ReadAllText(Path).Length > 0) ? true : false;
+    }
+
+    public static void ExcluirArquivo()
+    {
+        File.Delete(Path);
+    }
+
+
+    public static int LocalizarIndexJogo(string jogo)
+    {
+        int indexJogo = -1;
+        string[] jogosTexto = File.ReadAllLines(Path);
+       
+
+
+        for (int i = 0; i < jogosTexto.Length; i++)
+        {
+            if (jogosTexto[i].ToLower().Equals(jogo.ToLower()))
+            {
+                indexJogo = i;
+                return indexJogo;
+            }
+        }
+
+
+        return indexJogo;
+        
+    }
+
+    public static bool ExcluirJogo(string jogo)
+    {
+        bool jogoExcluido = false;
+        int indexJogo = LocalizarIndexJogo(jogo);
+
+        if (indexJogo == -1)
+        {
+            jogoExcluido = false;
+        }
+        else
+        {
+            string[] jogosTexto = File.ReadAllLines(Path);
+            jogosTexto[indexJogo+2] = null;
+            jogosTexto[indexJogo+1] = null;
+            jogosTexto[indexJogo] = null;
+            jogosTexto = RemoveAllEmptyValuesArray(jogosTexto);
+            AtualizarArquivo(jogosTexto);
+        }
+
+        return jogoExcluido;
+    }
+
+    public static void AtualizarArquivo(string[] jogos)
+    {
+        File.WriteAllLines(Path, jogos);
+    }
+
+    public static void AppendArquivo(string[] jogos)
+    {
+        File.AppendAllLines(Path, jogos);
+    }
+
+    public static string[] RemoveAllEmptyValuesArray(string[] array)
+    {
+        string[] result;
+        var temp = new List<string>();
+        foreach (var s in array)
+        {
+            if (!string.IsNullOrEmpty(s))
+                temp.Add(s);
+        }
+        result = temp.ToArray();
+        return result;
+    }
+
+    public static string GetPath()
+    {
+        return Path;
+    }
+
+}
---
+++ GerenciaGames/App_Code/Jogo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Jogo
+{
+    public string nome;
+    public string urlImagem;
+    public string sinopse;
+
+    public Jogo(string nome, string urlImagem, string sinopse)
+    {
+        this.nome = nome;
+        this.urlImagem = urlImagem;
+        this.sinopse = sinopse;
+    }
+
+    public void testeString()
+    {
+        System.Diagnostics.Debug.WriteLine(this.urlImagem);
+    }
+    
+
+}
